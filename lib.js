a814cd6808dc60fbd85420181d4606c644ab9677
Marek Zielina <niurop@tutamail.com>
2019-12-14 11:20:41 +0100

    It is finaly working, abit... Still needs work

--- lib.js
+++ lib.js
@@ -7,66 +7,74 @@ COS   = Math.cos
 EXP   = Math.exp
 LOG   = Math.log
 FLOOR = Math.floor
+CEIL  = Math.ceil
+CONST = function(x){return function(){return x}}
 
-function MAP (t, x0, y0, x1, y1){ return y1 + (y0 - t)/(y0 - x0) * (x1 - y1) }
-function MAPV(t, x0, y0, x1, y1){ return Vec3(MAP(t.x, x0.x, y0.x, x1.x, y1.x), MAP(t.y, x0.y, y0.y, x1.y, y1.y), MAP(t.z, x0.z, y0.z, x1.z, y1.z)) }
-function LERP (t, x, y){ return x + (y - x) * t }
-function LERPV(t, x, y){ return QADD(QSMultiple(t, SUB(y, x)), x) }
+//------------------------------------------------------------------------------
 
-function Vec3(x = 0, y = 0, z = 0){ return {x:x, y:y, z:z, u:1} }
-function Vec2(x = 0, y = 0){ return Vec3(x,y,0) }
-function Vec1(x = 0){ return Vec3(x,0,0) }
+function MAP  (t, x0, y0, x1, y1){ return y1 + (y0 - t)/(y0 - x0) * (x1 - y1) }
+function MAPV (t, x0, y0, x1, y1){ return Vec2(MAP(t.x, x0.x, y0.x, x1.x, y1.x), MAP(t.y, x0.y, y0.y, x1.y, y1.y)) }
+function QMAPV(t, x0, y0, x1, y1){ t.x = MAP(t.x, x0.x, y0.x, x1.x, y1.x); t.y = MAP(t.y, x0.y, y0.y, x1.y, y1.y); return t }
+function LERP  (t, x, y){ return x + (y - x) * t }
+function LERPV (t, x, y){ return QADD(SMultiple(t, SUB(y, x)), x) }
+function QLERPV(t, x, y){ return QADD(QSMultiple(t, SUB(y, x)), x) }
+function RLERP  (t, x, y){ return (t - x) / (y - x) }
+function RLERPV (t, x, y){ let T = SUB(t, x); let V = SUB(y, x); return Vec2(SProduct(T,V), SProduct(T, QPerpendicular(V)))}
+function QRLERPV(t, x, y){ QSUB(t, x); let V = SUB(y, x); return Vec2(SProduct(t,V), SProduct(t, QPerpendicular(V)))}
 
-EVec1 = Vec3(1, 0, 0)
-EVec2 = Vec3(0, 1, 0)
-EVec3 = Vec3(0, 0, 1)
+function Vec2(x = 0, y = 0){ return {x:x,y:y} }
+function Vec1(x = 0){ return {x:x,y:0} }
 
-function ADD (v, w){ return Vec3(v.x + w.x, v.y + w.y, v.z + w.z) }
-function QADD(v, w){ v.x += w.x; v.y += w.y; v.z += w.z; return v }
+EVec1 = Vec2(1, 0)
+EVec2 = Vec2(0, 1)
 
-function SUB (v, w){ return Vec3(v.x - w.x, v.y - w.y, v.z - w.z) }
-function QSUB(v, w){ v.x -= w.x; v.y -= w.y; v.z -= w.z; return v }
-function NEG (v){ return Vec3(-v.x, -v.y, -v.z) }
-function QNEG(v){ v.x = -v.x; v.y = -v.y; v.z = -v.z; return v }
+function ADD (v, w){ return {x:v.x + w.x, y:v.y + w.y} }
+function QADD(v, w){ v.x += w.x; v.y += w.y; return v }
 
-function CProduct (v, w) { return Vec2(v.x*w.x - v.y*w.y, v.x*w.y+v.y*w.x) }
+function SUB (v, w){ return {x:v.x - w.x, y:v.y - w.y} }
+function QSUB(v, w){ v.x -= w.x; v.y -= w.y; return v }
+function NEG (v){ return {x:-v.x, y:-v.y} }
+function QNEG(v){ v.x = -v.x; v.y = -v.y; return v }
+
+function CProduct (v, w) { return {x:v.x*w.x - v.y*w.y, y:v.x*w.y+v.y*w.x} }
 function QCProduct(v, w) { let x = v.x*w.x - v.y*w.y; v.y = v.x*w.y+v.y*w.x; v.x = x; return v }
-function SProduct (v, w) { return v.x*w.x + v.y*w.y + v.z*w.z }
-function VProduct (v, w) { return Vec3(v.y*w.z - v.z*w.y, v.z*w.x-v.x*w.z, v.x*w.y-v.y*w.x) }
-function QVProduct(v, w) { let x = v.y*w.z - v.z*w.y; let y = v.z*w.x-v.x*w.z; v.z = v.x*w.y-v.y*w.x; v.x = x; v.y = y; return v }
+function SProduct (v, w) { return v.x*w.x + v.y*w.y }
+function VProduct (v, w) { return v.x*w.y-v.y*w.x }
 
-function SMultiple(s, v) { return Vec3(v.x*s, v.y*s, v.z*s) }
-function QSMultiple(s, v){ v.x *= s; v.y *= s; v.z *= s; return v }
+function SMultiple (s, v){ return {x:v.x*s, y:v.y*s} }
+function QSMultiple(s, v){ v.x *= s; v.y *= s; return v }
 
-function CDiv (v, w){ let l = v.x*v.x+v.y*v.y; return Vec2((v.x*w.x+v.y*w.y)/l, (v.y*w.x-v.x*w.y)/l) }
+function CDiv (v, w){ let l = v.x*v.x+v.y*v.y; return {x:(v.x*w.x+v.y*w.y)/l, y:(v.y*w.x-v.x*w.y)/l} }
 function QCDiv(v, w){ let l = v.x*v.x+v.y*v.y; let x = (v.x*w.x+v.y*w.y)/l; v.y = (v.y*w.x-v.x*w.y)/l; v.x = x; return v }
-function CInvert (v){ let l = v.x*v.x+v.y*v.y; return Vec2(v.x / l, -v.y / l) }
+function CInvert (v){ let l = v.x*v.x+v.y*v.y; return {x:v.x / l, y:-v.y / l} }
 function QCInvert(v){ let l = v.x*v.x+v.y*v.y; v.x /= l; v.y /= -l; return v }
 
-function Conjugate(v) { return Vec2(v.x, -v.y) }
-function QConjugate(v){ return Vec2(v.x, -v.y) }
+function Conjugate (v){ return {x:v.x, y:-v.y} }
+function QConjugate(v){ v.y = -v.y; return v }
+function Perpendicular (v){ return {x:-v.y, y:v.x} }
+function QPerpendicular(v){ let x = v.x; v.x = -v.y; v.y = x; return v }
+function Projection  (v, w){ let s = v.x*w.x + v.y*w.y; return {x:w.x * s, y:w.y * s} }
+function QProjection (v, w){ let s = v.x*w.x + v.y*w.y; v.x = w.x*s; v.y = w.y*s; return v }
 
 function Len1(v){ return ABS(v.x) }
 function Len2(v){ return SQRT(v.x*v.x+v.y*v.y) }
-function Len3(v){ return SQRT(v.x*v.x+v.y*v.y+v.z*v.z) }
 
 function QLen1(v){ return v.x*v.x }
 function QLen2(v){ return v.x*v.x+v.y*v.y }
-function QLen3(v){ return v.x*v.x+v.y*v.y+v.z*v.z }
 
-function Unit1 (v){ return Vec1(v.x / ABS(v.x)) }
-function QUnit1(v){ v.x /= ABS(v.x); return v }
-function Unit2 (v){ let l = SQRT(v.x*v.x+v.y*v.y); return Vec2(v.x / l, v.y / l) }
-function QUnit2(v){ let l = SQRT(v.x*v.x+v.y*v.y); v.x /= l; v.y /= l; return v }
-function Unit3 (v){ let l = SQRT(v.x*v.x+v.y*v.y+v.z*v.z); return Vec3(v.x / l, v.y / l, v.z / l) }
-function QUnit3(v){ let l = SQRT(v.x*v.x+v.y*v.y+v.z*v.z); v.x /= l; v.y /= l; v.z /= l; return v }
-function VecABS(v){ return Vec3(ABS(v.x), ABS(v.y), ABS(v.z)) }
-function QVecABS(v){ v.x = ABS(v.x); v.y = ABS(v.y); v.z = ABS(v.z); return v }
+function Unit1  (v){ return {x:v.x / ABS(v.x), y:0} }
+function QUnit1 (v){ v.x /= ABS(v.x); return v }
+function Unit2  (v){ let l = SQRT(v.x*v.x+v.y*v.y); return {x:v.x / l, y:v.y / l} }
+function QUnit2 (v){ let l = SQRT(v.x*v.x+v.y*v.y); v.x /= l; v.y /= l; return v }
+function VecABS (v){ return {x:ABS(v.x), y:ABS(v.y)} }
+function QVecABS(v){ v.x = ABS(v.x); v.y = ABS(v.y); return v }
 
 function Matrix22(a, b, c, d){ return {M:[[a,b], [c,d]], x:2, y:2} }
 function Matrix33(a,b,c, d,e,f, g,h,j){ return {M:[[a,b,c], [c,d,e], [g,h,j]], x:3, y:3} }
-function Matrix44(a,b,c,d, e,f,g,h, i,j,k,l, m,n,s,t){ return {M:[[a,b,c,d], [e,f,g,h], [i,j,k,l], [m,n,s,t]], x:4, y:4} }
 function Matrix(data){ return {M:data, x:data[0].length, y:data.length} }
+function RotMatrix(alpha){ return {M:[[COS(alpha), SIN(alpha)], [-SIN(alpha), COS(alpha)]], x:2, y:2} }
+function RotAffMatrix(alpha){ return {M:[[COS(alpha), SIN(alpha), 0], [-SIN(alpha), COS(alpha), 0], [0, 0, 1]], x:3, y:3} }
+function TransAffMatrix(dx, dy){ return {M:[[0, 0, dx], [0, 0, dy], [0, 0, 1]], x:3, y:3} }
 
 function MulMM(A, B){
   let AM = A.M; BM = B.M
@@ -85,67 +93,127 @@ function MulMM(A, B){
   return {M:res, x:x, y:y}
 }
 
-function MulMV(A, v){
+function MulMV2(A, v){
   let AM = A.M
-  let x = MIN(A.x, 4)
-  let y = MIN(A.y, 4)
-  let res = [0,0,0,0]
-  if     (x == 1) for(i = 0; i < y; i++) res[i] = AM[i][0] * v.x
-  else if(x == 2) for(i = 0; i < y; i++) res[i] = AM[i][0] * v.x + AM[i][1] * v.y
-  else if(x == 3) for(i = 0; i < y; i++) res[i] = AM[i][0] * v.x + AM[i][1] * v.y + AM[i][2] * v.z
-  else if(x == 4) for(i = 0; i < y; i++) res[i] = AM[i][0] * v.x + AM[i][1] * v.y + AM[i][2] * v.z + AM[i][3] * v.u
-  if (y == 1) return Vec1(res[0])
-  if (y == 2) return Vec2(res[0], res[1])
-  if (y == 3) return Vec3(res[0], res[1], res[2])
-  if (y == 4) return Vec3(res[0]/res[3], res[1]/res[3], res[2]/res[3])
+  return Vec2(AM[0][0]*v.x + AM[0][1]*v.y, AM[1][0]*v.x + AM[1][1]*v.y)
 }
 
+function AffMV2(A, v){
+  let AM = A.M
+  return Vec2(AM[0][0]*v.x + AM[0][1]*v.y + AM[0][2], AM[1][0]*v.x + AM[1][1]*v.y + AM[1][2]) 
+}
 
+function ProjMV2(A, v){
+  let AM = A.M
+  let s = AM[2][0]*v.x+AM[2][1]*v.y+AM[2][2]
+  return Vec2((AM[0][0]*v.x + AM[0][1]*v.y + AM[0][2])/s, (AM[1][0]*v.x + AM[1][1]*v.y + AM[1][2])/s) 
+}
 
-// ####################################################### //
-// Ten interface jeszcze się znieni, to tylko szybkie demo //
-// Całość została napisana w jeden wieczór, więc ...       //
-// ####################################################### //
+//------------------------------------------------------------------------------
 
+function inBox(X, A, B)   { return X.x >= A.x && X.x <= B.x && X.y >= A.y && X.y <= B.y }
+function inCircle(X, p, r){ return QLen2(SUB(X,p)) <= r*r }
 
+//------------------------------------------------------------------------------
 
-function Canvas(width, height, display = true){
+Mouse = {pos:{x:0, y:0}, pressed:0, justPressed:0, justRelised:0}
+
+document.onmousemove = function(event){
+  Mouse.pos.x = event.clientX + document.body.scrollLeft
+  Mouse.pos.y = event.clientY + document.body.scrollTop
+}
+
+document.onmousedown = function(event){
+  Mouse.pressed     |= event.buttons
+  Mouse.justPressed |= event.buttons
+}
+
+document.onmouseup   = function(event){
+  Mouse.pressed     ^= Mouse.pressed & [0, 1, 4, 2][event.which]
+  Mouse.justRelised |= [0, 1, 4, 2][event.which]
+}
+
+Mouse.ButtonsDown = function(){
+  return {left  : 1 === (1 & Mouse.pressed),
+          right : 2 === (2 & Mouse.pressed),	    
+	  middle: 4 === (4 & Mouse.pressed)}
+  }
+Mouse.Pressed = function(){
+  let temp = {left  : 1 === (1 & Mouse.justPressed),
+              right : 2 === (2 & Mouse.justPressed),
+	      middle: 4 === (4 & Mouse.justPressed)}
+  Mouse.justPressed = 0
+  return temp
+}
+
+Mouse.Relised = function(){
+  let temp = {left  : 1 === (1 & Mouse.justRelised),
+              right : 2 === (2 & Mouse.justRelised),
+	      middle: 4 === (4 & Mouse.justRelised)}
+  Mouse.justRelised = 0
+  return temp
+}
+
+//------------------------------------------------------------------------------
+
+function Canvas(width, height, origin, scale){
   let canvas = document.createElement('canvas')
+  canvas.oncontextmenu = CONST(false)
   canvas.width  = width
   canvas.height = height
-  if(display)  document.body.appendChild(canvas)
-  return {obj:canvas, ctx:canvas.getContext('2d')}
-}
-
-function Slider(name, min, max, start, display = true){
-  let div       = document.createElement('div')
-  let range     = document.createElement('input')
-  range.type    = 'range'
-  range.min     = min
-  range.max     = max
-  range.value   = start
-  div.innerHTML = name
-  div.appendChild(range)
-  if(display)     document.body.appendChild(div)
-  return {obj:range, div:div}
-}
-
-function Button(name, display = true, callback = undefined){
-  let button = document.createElement('button')
-  button.innerHTML = name
-  if(display)  document.body.appendChild(button)
-  let self = {obj:button, callback:callback}
-  button.onclick = function(){
-    if(typeof self.callback !== 'undefined') self.callback()
+  let self = {obj:canvas, ctx:canvas.getContext('2d'), mouseInRange:false, width:width, height:height, size:MIN(width, height), scale:scale, origin:origin}
+  self.px = (width  - self.size)/2
+  self.py = (height - self.size)/2
+  self.mousePos = function(){
+    let pos = SUB(Mouse.pos, Vec2(canvas.offsetLeft, canvas.offsetTop))
+    self.mouseInRange = inBox(pos, Vec2(0,0), Vec2(width, height))
+    return pos
+  }
+  self.imagePos = function(vec){
+    return QADD(QConjugate(QSMultiple(self.scale/self.size*2, QSUB(vec, Vec2(self.width/2, self.height/2)))), self.origin)
   }
   return self
 }
 
-function Switch(cycle, display = true, callback = undefined){
+function Slider(min, max, start, f = function(x){return x}){
+  let range   = document.createElement('input')
+  range.type  = 'range'
+  range.min   = min
+  range.max   = max
+  range.value = start
+  range.setAttribute('class', 'slider')
+  let self = {obj:range, value:start}
+  self.hasChanged = function(){
+    let t = self.value != parseInt(self.obj.value)
+    self.value = parseInt(self.obj.value)
+    return t
+  }
+  self.getValue = function(){
+    self.value = parseInt(self.obj.value)
+    return self.value
+  }
+  return self
+}
+
+function Button(name){
   let button = document.createElement('button')
+  button.innerHTML = name
+  button.setAttribute('class', 'button')
+  let self = {obj:button, pressedCount: 0}
+  button.onclick = function(){ self.pressedCount++ }
+  self.Pressed = function(){
+    let t = self.pressedCount;
+    self.pressedCount = 0;
+    return t
+  }
+  return self
+}
+
+function Switch(cycle){
+  let button = document.createElement('button')
+  button.setAttribute('class', 'switch')
   button.innerHTML = cycle[0]
-  if(display)  document.body.appendChild(button)
-  let self = {obj:button, cycle:cycle, index:0, callback:callback}
+  let self = {obj:button, cycle:cycle, index:0, changed:false}
   self.setIndex = function(index){
     self.index  = index % self.cycle.length
     self.obj.innerHTML = self.cycle[index]
@@ -153,68 +221,130 @@ function Switch(cycle, display = true, callback = undefined){
   button.onclick = function(){
     self.index = (self.index + 1) % self.cycle.length
     self.obj.innerHTML = self.cycle[self.index]
-    if(typeof self.callback !== 'undefined') self.callback()
+    self.changed = true
+  }
+  button.oncontextmenu = function(){
+    self.index = (self.cycle.length + self.index - 1) % self.cycle.length
+    self.obj.innerHTML = self.cycle[self.index]
+    self.changed = true
+    return false
+  }
+  self.hasChanged = function(){
+    let t = self.changed
+    self.changed = false
+    return t
   }
   return self
 }
 
-function PaintingAnimation(size, domain, f){
-  let canvas    = Canvas(size[0], size[1], false)
-  let delay     = Slider('Delay', 0, 100, 0)
-  let batchsize = Slider('Batch', 1, MIN(256, MIN(size[0], size[1])), 100)
-  let self = {canvas:canvas, delay:delay, batchsize:batchsize, size:size, domain:domain, f:f, imagedata:canvas.ctx.getImageData(0,0,size[0], size[1]), StartStop:Switch(['Start', 'Stop'])}
-  self.loop = function(){
-    let data = self.imagedata.data
-    let w    = self.size[0]
-    let h    = self.size[1]
-    let s    = self.batchsize.obj.value|0
-    let x0   = (MAP(Math.random(), 0, 1, 0, (w/s-1))|0)*s
-    let y0   = (MAP(Math.random(), 0, 1, 0, (h/s-1))|0)*s
-    let end  = false
-    while(data[(x0 + y0*w)*4+3] === 0xff){
-      x0 += s
-      if(x0 >= w){
-        x0 = 0
-	y0 += s
-	if(y0 >= h){
-          y0 = 0
-	  if(end) return self.StartStop.setIndex(0)
-	  end = true
-    }}}
-    data[(x0 + y0*w)*4+3] = 0xff
-    if(self.StartStop.index === 1)
-      setTimeout(self.loop, self.delay.obj.value)
-    let w0 = MIN(x0 + s, w)
-    let h0 = MIN(y0 + s, h)
-    let f  = self.f
-    let y, x, RGB, pos
-    for(j = y0; j < h0; j++){
-      y = MAP(j, 0, h, self.domain[2], self.domain[3])
-      for(i = x0; i < w0; i++){
-        x = MAP(i, 0, w, self.domain[0], self.domain[1])
-        RGB = f(x, y)
-	pos = (i + j*w) * 4
-        data[pos+0] = RGB[0]
-        data[pos+1] = RGB[1]
-        data[pos+2] = RGB[2]
-        data[pos+3] = 0xff
-    }}
-    self.canvas.ctx.putImageData(self.imagedata, 0, 0, x0, y0, s, s)
+function TextInput(columns, lines, start){
+  let input = document.createElement('textarea')
+  let self  = {obj:input, value:start}
+  input.setAttribute('class', 'textInput')
+  input.cols = columns
+  input.rows = lines
+  input.style.resize = 'none'
+  input.value = start
+  self.hasChanged = function(){
+    let t = (self.obj.value != self.value)
+    self.value = self.obj.value
+    return t
   }
-  self.reset = function(){
-    let data = self.imagedata.data
-    let w  = self.size[0]
-    let h  = self.size[1]
-    for(let x = 0; x < w; x++)for(let y = 0; y < h; y++)
-      data[(x+y*w)*4+3] = data[(x+y*w)*4+3] ? 0xFE : 0
-    self.canvas.ctx.putImageData(self.imagedata, 0, 0)
+  self.getText = function(){
+    self.value = self.obj.value
+    return self.value
   }
-  self.StartStop.callback = function(){if(self.StartStop.index === 1) self.loop()}
-  self.reset = Button('Override', true, self.reset)
-  document.body.appendChild(self.canvas.obj)
+  self.setText = function(str){
+    input.value = str
+    self.value  = str
+  }
   return self
 }
 
-function RGBfromVec3(v){ return [(ABS(v.x)%256)|0, (ABS(v.y)%256)|0, (ABS(v.z)%256)|0] }
+function Text(str){
+  let text = document.createElement('textarea')
+  text.setAttribute('class', 'text')
+  text.value = str
+  text.style.resize = 'none'
+  text.setAttribute('readonly', true)
+  text.cols = str.length
+  text.rows = 1
+  let self = {obj:text}
+  self.setText = function(str){
+    text.value = str
+  }
+  return self
+}
+
+//------------------------------------------------------------------------------
+
+function AppendOBJ(obj, div = document.body){
+  div.appendChild(obj.obj ? obj.obj : obj)
+}
+
+function CreatePresentation(options, origin = Vec2(0,0), scale = 1){
+  let DIV     = document.createElement('div')
+  let MAIN    = document.createElement('div')
+  let OPTIONS = document.createElement('div')
+  DIV.    setAttribute('class', 'presentation')
+  MAIN.   setAttribute('class', 'main')
+  OPTIONS.setAttribute('class', 'options')
+  AppendOBJ(MAIN,    DIV)
+  AppendOBJ(OPTIONS, DIV)
+  AppendOBJ(DIV)
+  let CANVAS = Canvas(MAIN.clientWidth, MAIN.clientHeight, origin, scale)
+  AppendOBJ(CANVAS, MAIN)
+  CANVAS.obj.setAttribute('class', 'center')
+  let self = {obj:DIV, can:CANVAS, opt:OPTIONS}
+  self.addOptions = function(options){
+    options.forEach(function(list){
+      if(!list.format){
+        list.forEach(function(o){ 
+	  o.obj.style.width = '100%'
+          AppendOBJ(o, OPTIONS)
+        })
+      } else if(list.format === 'justify'){
+        let len = list.elems.length
+        list.elems.forEach(function(o){ 
+          o.obj.style.width = (100/len - 1) + '%'
+          AppendOBJ(o, OPTIONS)
+        })
+      } else if(list.format === 'alignLeft'){
+        list.elems.forEach(function(o){ 
+	  o.obj.style.float = 'left'
+	  o.obj.style.verticalalign = 'top'
+          AppendOBJ(o, OPTIONS)
+        })
+	/* It does not work as intended
+	let width = 0
+	console.log(width)
+        list.elems.forEach(function(o){
+	  console.log(o.obj.clientwidth) 
+	  width += o.obj.clientwidth
+	})
+	console.log(width)
+        list.elems.forEach(function(o){
+	  o.obj.style.width = (width / o.obj.clientwidth) + '%'
+	})*/
+      }
+      AppendOBJ(document.createElement('br'), OPTIONS)
+    })
+  }
+  self.addOptions(options)
+  return self
+}
+
+//------------------------------------------------------------------------------
+
+function QRGB(r,g,b){ return [r, g, b] }
+function RGB (r,g,b){ return [(r%256)|0, (g%256)|0, (b%256)|0] }
+function NRGB(r,g,b){ return [(r*256)|0, (g*256)|0, (b*256)|0] }
+function QGray(l){ return [l, l, l] }
+function Gray (l){ return RGB (l,l,l) }
+function NGray(l){ return NRGB(l,l,l) }
+
+function ColorLerp(t, A, B){ return [LERP(t, A[0], B[0]), LERP(t, A[1], B[1]), LERP(t, A[2], B[2])] }
+
+//------------------------------------------------------------------------------
 
 
--- mandelbrot.js
+++ mandelbrot.js
@@ -1,12 +1,91 @@
-len = 500
-f = function(x, y){
-  if(x < .25 && x > -.5 && y < .5 && y > -.5)
-    return RGBfromVec3(Vec3(0,0,0))
-  let c = Vec2(x,y)
-  let v = Vec2(x,y)
-  let i = 0
-  for(; QLen2(v) <= 4 && i < len; i++)
-    QADD(QCProduct(v, v), c)
-  return RGBfromVec3(LERPV(SQRT(SQRT(SQRT(i / (len+1)))), Vec3(0, 230, 170), Vec3(0, 0, 0)))
+UPDATE = Button('Update')
+X0     = TextInput(5, 1, '-0.75')
+Y0     = TextInput(5, 1, '0')
+S      = TextInput(5, 1, '1')
+MOUSE  = Text('0, 0')
+MANDELBROT = Switch(['Mandelbrot', 'Hello', 'Buhahaha'])
+NSLIDER = Slider(1, 1000, 50)
+
+Functions = ['c = Vec2(x, y)\nv = Vec2()\nfor(i = 0; i < N && QLen2(v) <= 4; i++)\nQADD(QCProduct(v,v), c)\nF = i / N',
+ 'F = x + y',
+ 'F = x - y']
+
+F = TextInput(30, 10, Functions[0])
+
+HELP = Text('HELP')
+Options = [
+  {elems:[UPDATE, MANDELBROT], format:'justify'},
+  {elems:[Text('(x, y) ='), X0, Y0, Text('ε ='), S], format:'alignLeft'},
+  [MOUSE],
+  [NSLIDER],
+  {elems:[Text('F(x,y) = '), F], format:'alignLeft'},
+  [HELP]
+]
+
+State   = {}
+P = CreatePresentation(Options, Vec2(), 1)
+State.imagePos  = P.can.imagePos
+State.imageData = P.can.ctx.getImageData(0, 0, P.can.width, P.can.height)
+State.size = 64
+State.W    = CEIL(P.can.width  / State.size)
+State.H    = CEIL(P.can.height / State.size)
+State.N    = 100
+
+Update = function(){
+  State.N = parseFloat(NSLIDER.value)
+  let N = State.N
+  eval('State.f = function(x,y){\nlet F = 0\n' + F.getText() + '\n return F}')
+  P.can.origin.x = parseFloat(X0.getText())
+  P.can.origin.y = parseFloat(Y0.getText())
+  P.can.scale    = parseFloat( S.getText())
+  State.i = 0
 }
-P = PaintingAnimation([4000, 4000], [-2, 1, -1.3, 1.3], f) 
+
+loop = function(){
+  let pos = P.can.imagePos(P.can.mousePos())
+  if(MANDELBROT.hasChanged()){
+    F.setText(Functions[MANDELBROT.index])
+    Update()
+  }
+  if(UPDATE.Pressed()){
+    Update()
+  }
+  mouse = Mouse.Relised()
+  if(mouse.left && P.can.mouseInRange){
+    X0.setText(pos.x)
+    Y0.setText(pos.y)
+    Update()
+  }
+  if(NSLIDER.hasChanged()){
+    Update()
+  }
+  if(State.i <= State.W * State.H){
+    let data     = State.imageData.data
+    let imagePos = P.can.imagePos
+    let s  = State.size
+    let w  = P.can.width
+    let h  = P.can.height
+    let x  = s * (State.i % State.W)
+    let y  = s * FLOOR(State.i / State.W)
+    let w0 = MIN(x + s, w)
+    let h0 = MIN(y + s, h)
+    let f  = State.f
+    for(let j = y; j < h0; j++){
+      for(let i = x; i < w0; i++){
+      let X = imagePos(Vec2(i, j))
+      let c = NGray(f(X.x, X.y))
+      let P0 = (i + j*w)*4
+      data[P0+0] = c[0]
+      data[P0+1] = c[1]
+      data[P0+2] = c[2]
+      data[P0+3] = 0xff
+    }}
+    P.can.ctx.putImageData(State.imageData, 0, 0, x, y, s, s)
+    State.i++;
+  }
+  MOUSE.setText(JSON.stringify(pos))
+  setTimeout(loop, 1)
+}
+
+Update()
+loop()
